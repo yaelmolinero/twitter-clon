981402f6626b66f00381866c21426056e2f7f140
Yael Molinero <y.molinero20@gmail.com>
2025-08-22 21:49:45 -0600

    Update ESLint config to ignore 'server/dist' directory

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
+  { ignores: ['dist', 'node_modules', 'server/dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx,js,jsx}'],
